b3f41786f45803ef498a9445e7215e64fa9c9f76
Jarek <jarekjanas95@gmail.com>
2021-12-29 19:41:14 +0100

    airporttransfer

--- src/pages/AirportTransfer.js
+++ src/pages/AirportTransfer.js
@@ -6,6 +6,11 @@ import "aos/dist/aos.css";
 
 import { FaLongArrowAltRight } from "react-icons/fa";
 
+import shuttleGuest from "../images/inne/shuttleGuest.jpg";
+import wieliczkaImg from "../images/inne/wieliczka.jpg";
+import auschwitzImg from "../images/inne/auschwitz.jpg";
+import zakopaneImg from "../images/inne/zakopane.jpg";
+
 const AirportTransfer = () => {
   useEffect(() => {
     Aos.init({ duration: 1000 });
@@ -18,7 +23,7 @@ const AirportTransfer = () => {
           <h2>Transfery i Wycieczki</h2>
         </div>
       </div>
-      <div className="shuttleAndTours">
+      <div className="shuttleContainer">
         <div className="shuttle">
           <h3>Transfery Lotniskowe</h3>
           <p>
@@ -27,10 +32,14 @@ const AirportTransfer = () => {
           </p>
           <span>
             {" "}
-            <FaLongArrowAltRight /> 1-3 osoby: 90 zł
+            <FaLongArrowAltRight className="icon" /> 1-3 osoby: 90 zł
           </span>
-          <span>4-6 osób: 140 zł</span>
-          <span>7-8 osób: 160 zł</span>
+          <span>
+            <FaLongArrowAltRight className="icon" /> 4-6 osób: 140 zł
+          </span>
+          <span>
+            <FaLongArrowAltRight className="icon" /> 7-8 osób: 160 zł
+          </span>
           <p>Powyżej ośmiu osób koszt transferu obliczamy indywidualnie.</p>
           <p>
             Aby dokonać rezerwacji transportu prosimy o podanie: numeru lotu,
@@ -41,7 +50,49 @@ const AirportTransfer = () => {
             poinformować nas o konieczności przygotowania fotelika dziecięcego.
           </p>
         </div>
-        <div className="tours"></div>
+        <img
+          data-aos="fade-right"
+          data-aos-offset="300"
+          src={shuttleGuest}
+          alt="airport"
+        />
+      </div>
+      <div className="toursContainer">
+        <div className="imgContainer">
+          <span data-aos="flip-right">
+            <img src={auschwitzImg} alt="auschwitz" />
+            <h4>Auschwitz</h4>
+          </span>
+          <span data-aos="flip-right">
+            <img src={wieliczkaImg} alt="wieliczka" />
+            <h4>Wieliczka</h4>
+          </span>
+          <span data-aos="flip-right">
+            <img src={zakopaneImg} alt="zakopane" />
+            <h4>Zakopane</h4>
+          </span>
+        </div>
+        <div className="tours">
+          <h3>Wycieczki</h3>
+          <p>Organizujemy wycieczki:</p>
+          <span>
+            <FaLongArrowAltRight className="icon" /> do Auchwitz Birkenau: koszt
+            od 180 zł za osobę,
+          </span>
+          <span>
+            <FaLongArrowAltRight className="icon" /> do Kopalni Soli w
+            Wieliczce: koszt od 180 zł za osobę,
+          </span>
+          <span>
+            <FaLongArrowAltRight className="icon" /> do Zakopanego: koszt od 350
+            zł za osobę,
+          </span>
+          <p>
+            Opłata obejmuje: transfer do celu i z powrotem do hotelu,
+            przewodnika w języku angielskim, włoskim, hiszpańskim, francuskim
+            lub niemieckim, bilet wstępu.
+          </p>
+        </div>
       </div>
     </Wrapper>
   );
@@ -77,12 +128,16 @@ const Wrapper = styled.div`
       }
     }
   }
-  .shuttleAndTours {
+  .shuttleContainer {
     display: flex;
     justify-content: space-around;
     align-items: center;
     width: 80vw;
-    margin: 10vh auto;
+    margin: 15vh auto;
+    img {
+      width: 300px;
+      border-radius: 3px;
+    }
     .shuttle {
       display: flex;
       flex-direction: column;
@@ -106,7 +161,103 @@ const Wrapper = styled.div`
       font-size: 1rem;
       line-height: 1.3;
       color: var(--secondaryColor2);
-      margin-left: 2vw;
+      margin: 0.5rem 0 0.5rem 2vw;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      font-weight: bold;
+      .icon {
+        margin-right: 10px;
+      }
+    }
+  }
+  .toursContainer {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 80vw;
+    margin: 20vh auto 30vh;
+    .imgContainer {
+      width: 50%;
+      height: 50vh;
+      position: relative;
+      span {
+        width: 300px;
+        height: 180px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        border-radius: 3px;
+        :hover img {
+          filter: brightness(0.3);
+        }
+        :hover h4 {
+          opacity: 1;
+        }
+      }
+      span:nth-of-type(2) {
+        top: 33%;
+        left: 50%;
+      }
+      span:nth-of-type(3) {
+        top: 66%;
+        left: 0%;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        border-radius: 3px;
+        transition: 0.4s;
+        border: 1px solid var(--secondaryColor2);
+      }
+      h4 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-family: var(--buttonFont);
+        font-size: 2rem;
+        opacity: 0;
+        transition: 0.4s;
+      }
+    }
+    .tours {
+      display: flex;
+      flex-direction: column;
+      width: 40%;
+      align-items: flex-start;
+      justify-content: center;
+      h3 {
+        font-family: var(--buttonFont);
+        font-size: 2rem;
+        margin-bottom: 3vh;
+        color: var(--secondaryColor2);
+      }
+      p {
+        font-family: var(--textFont);
+        font-size: 1rem;
+        line-height: 1.3;
+        margin: 3vh auto 3vh 0;
+        text-align: left;
+      }
+      span {
+        font-family: var(--textFont);
+        font-size: 1rem;
+        line-height: 1.3;
+        color: var(--secondaryColor2);
+        margin: 0.5rem 0 0.5rem 2vw;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        font-weight: bold;
+        .icon {
+          margin-right: 10px;
+        }
+      }
     }
   }
 `;
